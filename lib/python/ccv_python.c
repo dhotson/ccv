46415a2712b7305b0be90e71a5aa9afb9ddfaf2a
Dennis Hotson <dennis@99designs.com>
2012-09-24 10:21:06 +1000

    Decrement references set with PyDict_SetItem

--- lib/python/ccv_python.c
+++ lib/python/ccv_python.c
@@ -14,17 +14,17 @@ py_ccv_sift(PyObject *self, PyObject *args)
     ccv_dense_matrix_t* image = 0;
     ccv_read(filename, &image, CCV_IO_GRAY | CCV_IO_ANY_FILE);
 
-	ccv_sift_param_t params = {
-		.noctaves = 3,
-		.nlevels = 6,
-		.up2x = 1,
-		.edge_threshold = 10,
-		.norm_threshold = 0,
-		.peak_threshold = 0,
-	};
-	ccv_array_t* image_keypoints = 0;
-	ccv_dense_matrix_t* image_desc = 0;
-	ccv_sift(image, &image_keypoints, &image_desc, 0, params);
+    ccv_sift_param_t params = {
+        .noctaves = 3,
+        .nlevels = 6,
+        .up2x = 1,
+        .edge_threshold = 10,
+        .norm_threshold = 0,
+        .peak_threshold = 0,
+    };
+    ccv_array_t* image_keypoints = 0;
+    ccv_dense_matrix_t* image_desc = 0;
+    ccv_sift(image, &image_keypoints, &image_desc, 0, params);
 
     PyObject* keypoints = PyList_New(image_keypoints->rnum);
     PyObject* descriptors = PyList_New(image_keypoints->rnum);
@@ -51,11 +51,13 @@ py_ccv_sift(PyObject *self, PyObject *args)
     PyObject* ret = PyDict_New();
     PyDict_SetItemString(ret, "keypoints", keypoints);
     PyDict_SetItemString(ret, "descriptors", descriptors);
+    Py_DECREF(keypoints);
+    Py_DECREF(descriptors);
 
-	ccv_array_free(image_keypoints);
-	ccv_matrix_free(image_desc);
-	ccv_matrix_free(image);
-	ccv_disable_cache();
+    ccv_array_free(image_keypoints);
+    ccv_matrix_free(image_desc);
+    ccv_matrix_free(image);
+    ccv_disable_cache();
     return ret;
 }
 
